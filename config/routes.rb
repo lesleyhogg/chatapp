5bb0c73595ddcd06c44b1e25ef1fd13abe995303
Lesley Hogg <lesley@greenriver.org>
2018-10-30 13:37:50 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :messages, only: [:index]
+  resources :sessions, only: [:new, :create]
+
+  root 'sessions#new'
+
+  mount ActionCable.server => '/cable'
+end
